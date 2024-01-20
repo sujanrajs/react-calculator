6dd8b439560643b76e59e4863598ebab667f4412
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2024-01-20 22:20:50 +0200

    added logic for DEL button

--- src/App.js
+++ src/App.js
@@ -61,6 +61,26 @@ function reducer(state, { type, payload }) {
       };
     case ACTIONS.CLEAR:
       return {};
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overWrite) {
+        return {
+          ...state,
+          overWrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
@@ -122,7 +142,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
