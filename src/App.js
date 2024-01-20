60a4396113bf51d38f5655d23f468889bfe5464f
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2024-01-20 22:32:01 +0200

    added logic to format numbers to seperate by commas

--- src/App.js
+++ src/App.js
@@ -123,6 +123,17 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString();
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
@@ -134,7 +145,7 @@ function App() {
         <div className="previous-operand">
           {previousOperand} {operation}
         </div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button
         className="span-two"
